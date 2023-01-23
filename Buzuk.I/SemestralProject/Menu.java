19e35dc54e72bae7c5e78a74fd1ca0468c7ade9f
Ivan Buzuk <115068113+zukmitsense@users.noreply.github.com>
2023-01-23 12:20:57 +0100

    Add files via upload

---
+++ Buzuk.I/SemestralProject/Menu.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ortonormalnivektora;
+
+import static ortonormalnivektora.OrtonormalniVektora.sc;
+
+/**
+ *
+ * @author ivan
+ */
+public class Menu {
+    public static void main(String[] args){
+        boolean cycle;
+        cycle = true;
+        while(cycle){
+          menu();  
+        }
+        
+    }
+    private static void menu(){
+        System.out.println("Jaky program chcete spustit?");
+        System.out.println("[1] OrtonormalniVektora     [2] VanocniUloha ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1 -> OrtonormalniVektora.ortonormalniVektora();
+            case 2 -> ortonormalnivektora.ChristmasBuzukIvan.Vanocni_uloha();
+            default -> System.out.println("Nevalidni vstup");
+                    
+                    
+        }
+        
+    }
+}
