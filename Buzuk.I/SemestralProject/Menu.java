74014833e943418911289d9cb3d7c9d70e9a1313
Ivan Buzuk <115068113+zukmitsense@users.noreply.github.com>
2023-01-23 12:22:45 +0100

    Update Menu.java

--- Buzuk.I/SemestralProject/Menu.java
+++ Buzuk.I/SemestralProject/Menu.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ortonormalnivektora;
+package SemestralProject;
 
-import static ortonormalnivektora.OrtonormalniVektora.sc;
+import static SemestralProject.OrtonormalniVektora.sc;
 
 /**
  *
